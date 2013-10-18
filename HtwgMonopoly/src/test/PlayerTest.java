6a244b74085ddca8dbf776d9c335b68f82c444f0
stgorenf <stgorenf@3DLABOR17.in.fhkn.de>
2013-10-18 16:22:13 +0200

    added testcases for class "Player".

---
+++ HtwgMonopoly/src/test/PlayerTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayerString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+}
