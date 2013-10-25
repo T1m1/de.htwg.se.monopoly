a8f964fdb57320eea4f45034fae89ad45ccd1285
RuprechtT <Ruprecht.t@gmx.de>
2013-10-25 12:24:48 +0200

    added testfolder

---
+++ HtwgMonopoly/test/PlayerTest.java
@@ -0,0 +1,74 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testPlayerStringCharDoubleStringArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayerStringCharDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFigure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFigure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBudget() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBudget() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOwnership() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOwnership() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPosition() {
+		fail("Not yet implemented");
+	}
+
+}
