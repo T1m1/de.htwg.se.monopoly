8b1f3c49bab414e90a26d2cb7105e50d18bbdd69
Steffen Gorenflo <SteffenGorenflo@gmail>
2013-10-25 17:25:37 +0200

    Dice modified

--- HtwgMonopoly/src/de/htwg/monopoly/entities/Dice.java
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/Dice.java
@@ -3,11 +3,25 @@ package de.htwg.monopoly.entities;
 public class Dice {
 	private int dice;
 
+	public Dice() {
+		setDice(1, 6);
+	}
 	public int getDice() {
 		return dice;
 	}
 
+	/**
+	 * Set the dice with a random number between lowerbound and upperbound (inclusive)
+	 * @param lowerBound
+	 * @param upperBound
+	 */
 	public void setDice(int lowerBound, int upperBound) {
+		/* if lowerBound is greater the upperBound, than switch the values */
+		if (lowerBound > upperBound) {
+			int a = lowerBound;
+			lowerBound = upperBound;
+			upperBound = a;
+		}			
 		upperBound++;
 		this.dice = (int) (Math.random() * (upperBound - lowerBound) + lowerBound );
 	}
