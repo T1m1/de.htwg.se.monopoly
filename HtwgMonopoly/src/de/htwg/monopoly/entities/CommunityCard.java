9d46eec052c98e7efe38b2bcf5e642585d1cb89f
Steffen Gorenflo <SteffenGorenflo@gmail>
2013-10-25 18:01:54 +0200

    CommunityCard class and CommunityCardTest class added

---
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/CommunityCard.java
@@ -0,0 +1,18 @@
+package de.htwg.monopoly.entities;
+
+public class CommunityCard {
+	private String description;
+	
+	public CommunityCard(String description) {
+		setDescription(description);
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
