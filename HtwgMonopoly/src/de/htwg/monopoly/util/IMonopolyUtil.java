76a81e99931c1841fe37c643a7e5541b350e9b51
RuprechtT <Ruprecht.t@gmx.de>
2013-11-08 17:50:05 +0100

    fixed util issues

--- HtwgMonopoly/src/de/htwg/monopoly/util/IMonopolyUtil.java
+++ HtwgMonopoly/src/de/htwg/monopoly/util/IMonopolyUtil.java
@@ -1,11 +1,11 @@
 package de.htwg.monopoly.util;
 
 public interface IMonopolyUtil {
-	public static final int NULL = 0;
-	public static final int ONE = 1;
-	public static final int TEST_PRICE_ONE = 250;
-	public static final int TEST_PRICE_TWO = 230;
-	public static final int TEST_PRICE_THREE = 50;
+	static final int NULL = 0;
+	static final int ONE = 1;
+	static final int TEST_PRICE_ONE = 250;
+	static final int TEST_PRICE_TWO = 230;
+	static final int TEST_PRICE_THREE = 50;
 	
 
 }
