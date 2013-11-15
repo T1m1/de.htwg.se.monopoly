49eda20824015241398aaa6f75b31496ce37340c
stgorenf <stgorenf@3DLAB13.in.fhkn.de>
2013-11-15 17:28:32 +0100

    card container is fertig

--- HtwgMonopoly/src/de/htwg/monopoly/entities/CardsContainer.java
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/CardsContainer.java
@@ -7,8 +7,33 @@ public class CardsContainer {
 
 	private Deque<Card> chanceCards = new LinkedList<Card>();
 	private Deque<Card> commuCards = new LinkedList<Card>();
-	
+
 	public CardsContainer() {
-		//chanceCards.push(new Card()); //TODO elemente und Inhalte (Texte) hinzufügen
+		// chanceCards.push(new Card()); //TODO elemente und Inhalte (Texte)
+		// hinzufügen
+	}
+
+	/**
+	 * Return the first Card off the Chance cardstack and put it back under the
+	 * stack. Not yet implemented: "Du kommst aus dem Gefängnis frei"-Karte 
+	 * 
+	 * @return Card
+	 */
+	public Card getNextChanceCard() {
+		Card tmp = chanceCards.pollFirst();
+		chanceCards.offerLast(tmp);
+		return tmp;
+	}
+
+	/**
+	 * Return the first Card off the Community Chest cardstack and put it back
+	 * under the stack "Du kommst aus dem Gefängnis frei"-Karte 
+	 * 
+	 * @return Card
+	 */
+	public Card getNextCommuCard() {
+		Card tmp = commuCards.pollFirst();
+		commuCards.offerLast(tmp);
+		return tmp;
 	}
 }
