de448362c973e5dee73a32759400e4b8bb6a3cdf
stgorenf <stgorenf@3DLAB13.in.fhkn.de>
2013-11-15 18:11:55 +0100

    Klasse ereigniskarten und gemeinschafts karten erstellt

---
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/CommunityCard.java
@@ -0,0 +1,17 @@
+package de.htwg.monopoly.entities;
+
+public class CommunityCard implements ICards{
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getActionType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
