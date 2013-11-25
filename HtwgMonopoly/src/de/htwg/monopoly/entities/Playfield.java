271abe2f598c741d8fea55750154f4691edae5e1
stgorenf <stgorenf@3DLAB14.in.fhkn.de>
2013-11-25 18:17:14 +0100

    first approach of the plafield class done

--- HtwgMonopoly/src/de/htwg/monopoly/entities/Playfield.java
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/Playfield.java
@@ -3,16 +3,21 @@ package de.htwg.monopoly.entities;
 public class Playfield {
 	
 	private IFieldObject[] playfield;
+	private IFieldObject currentFieldObject;
 	
 	public void initialize() {
 		//initialize the playfield. set the size, fill it with streets and card stacks etc.
 	}
 	
-	public void performAction(Player currentPlayer, int diceResult) {
-		int position = currentPlayer.getPosition();
-		position = position + diceResult;
-		// if position greater than actual playfield size do magic stuff
+	public boolean performAction(Player currentPlayer, int diceResult) {
+		// calculate the new position of the player within the playfield range
+		int position = (currentPlayer.getPosition() + diceResult) % playfield.length;
 		
+		// set the current FieldObject, where the Player is right now
+		this.currentFieldObject = this.playfield[position];
+		
+		// return true, if the Player went over or stays on "Los"
+		return (position < currentPlayer.getPosition());		
 	}
 	
 }
