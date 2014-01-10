fdc2f84ec12961b97a7a19e6e7f8105b56baa89f
stgorenf <stgorenf@3DLAB13.in.fhkn.de>
2014-01-10 14:22:46 +0100

    added action class

---
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/Action.java
@@ -0,0 +1,12 @@
+package de.htwg.monopoly.entities;
+
+
+
+public class Action {
+
+	public static void perform(ICards currentCard, Player currentPlayer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
