42430e1aeb9c01ccb3a3fbd33302d689d8a50659
SteffenGorenflo <stgorenf@3DLAB18.htwg-konstanz.de>
2014-12-15 19:10:53 +0100

    remove unused game information class, add question for prison

--- HtwgMonopoly/src/de/htwg/monopoly/entities/impl/PrisonQuestion.java
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/impl/PrisonQuestion.java
@@ -7,28 +7,45 @@ import java.util.TreeMap;
 
 public class PrisonQuestion {
 
-	private static Map<String,Boolean> questionMap = new TreeMap<String,Boolean>();
-	
+	private static Map<String, Boolean> questionMap = new TreeMap<String, Boolean>();
+
 	private static LinkedList<String> questions;
-	
+
 	static {
-		questionMap.put("", true);
-		questionMap.put("", true);
-		questionMap.put("", true);
-		questionMap.put("", true);
-		questionMap.put("", true);
-		questionMap.put("", true);
-		questionMap.put("", true);
-		questionMap.put("", false);
-		questionMap.put("", false);
-		questionMap.put("", false);
-		questionMap.put("", false);
-		questionMap.put("", false);
-		questionMap.put("", false);
-		
-		questions = new  LinkedList<String>(questionMap.keySet());
+		questionMap.put(
+				"Gibt es einen Unterschied zwischen Kernel und User threads?",
+				true);
+		questionMap
+				.put("Gibt es einer Unterschied zwischen einer Seite und einem Segment?",
+						true);
+		questionMap
+				.put("Ist eine Leistungssteigerung zu erwarten, wenn statt eines einzelnen Pufferspeichers für die E/A zwei Pufferspeicher verwendet werden?",
+						true);
+		questionMap
+				.put("Gibt es einen Unterschiedd zwischen einem Trap und einer Unterbrechung (Interrupt)?",
+						true);
+		questionMap
+				.put("Das Client-Server-Modell ist bei verteilten Systemen sehr verbreitet. Kann es auch in einem Einplatz-Computer Anwendung finden?",
+						true);
+		questionMap
+				.put("Kann ein Benutzer einen eigenen Kommandozeileninterpreter entwickeln, indem er das Systemcall-Interface des Kernels benutzt?",
+						true);
+		questionMap.put("Ist Multiprogramming und Multitasking das gleiche?",
+				false);
+		questionMap
+				.put("Sollte der Befehl die aktuelle Uhrzeit zu lesen nur im Kernmodus erlaubt sein?",
+						false);
+		questionMap
+				.put("Eine Bibliotheksfunktion hat den Namen read(). Der Systemaufruf des Betriebssystems heißt auch read(). Ist es nötig, dass beide Funktionen denselben Namen haben?",
+						false);
+		questionMap.put("ISRs sollten eher lang sein.", false);
+		questionMap
+				.put("Kann eine Multithreading-Anwendung mit mehreren User-Level Threads auf einem Multiprozessorsystem eine bessere Performance erreichen als auf einem Single-Prozessorsystem? ",
+						false);
+
+		questions = new LinkedList<String>(questionMap.keySet());
 	}
-	
+
 	public PrisonQuestion() {
 		Collections.shuffle(questions);
 	}
