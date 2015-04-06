cdc9a605e0b0c2d172bef790beead64676fd7e8b
T1m1 <Ruprecht.t@gmx.de>
2015-04-06 21:19:15 +0200

    remove deprecated constructor

--- HtwgMonopoly/src/de/htwg/monopoly/controller/impl/PlayerController.java
+++ HtwgMonopoly/src/de/htwg/monopoly/controller/impl/PlayerController.java
@@ -14,162 +14,132 @@ import de.htwg.monopoly.util.PlayerIcon;
 
 /**
  * Controller for player
- * 
- * 
  */
 public class PlayerController implements IPlayerController {
 
-	// array with all players
-	private List<Player> players;
+    // array with all players
+    private List<Player> players;
 
-	// the number of players
-	private int numberOfPlayer;
+    // the number of players
+    private int numberOfPlayer;
 
-	// the player of the current turn
-	private int currentPlayer;
+    // the player of the current turn
+    private int currentPlayer;
 
-	/**
-	 * Constructor initialize all player with default values. See {@link Player}
-	 * for more information on Player.
-	 * 
-	 * @param nameOfPlayers
-	 * @param inNumberOfPlayers
-	 * @deprecated Replaced by the constructor receiving a map.
-	 */
-	@Deprecated
-	public PlayerController(int inNumberOfPlayers, String[] nameOfPlayers) {
-
-		// check correct number of players
-		assert nameOfPlayers.length != inNumberOfPlayers : "Anzahl der Spieler and Anzahl der Spielernamen stimmt nicht �berein";
-
-		if (!MonopolyUtils.verifyPlayerNumber(inNumberOfPlayers)) {
-			throw new AssertionError("Ung�ltige Anzahl an Spielern.");
-		}
-
-		// set number of players
-		this.numberOfPlayer = inNumberOfPlayers;
-
-		// create array of players.
-		this.players = new ArrayList<Player>();
-
-		for (String currentName : nameOfPlayers) {
-			players.add(new Player(currentName));
-		}
-
-	}
 
     /**
      * Constructor initialize all player with default values. See {@link Player}
      * for more information on Player
      * *
+     *
      * @param inPlayers all players as list
      */
-	public PlayerController(List<String> inPlayers) {
-		this.numberOfPlayer = inPlayers.size();
+    public PlayerController(List<String> inPlayers) {
+        this.numberOfPlayer = inPlayers.size();
 
-		// assert wrong number.
-		if (!MonopolyUtils.verifyPlayerNumber(numberOfPlayer)) {
-			throw new AssertionError("Ung�ltige Anzahl an Spielern.");
-		}
+        // assert wrong number.
+        if (!MonopolyUtils.verifyPlayerNumber(numberOfPlayer)) {
+            throw new AssertionError("Ung�ltige Anzahl an Spielern.");
+        }
 
-		// create array of players.
-		this.players = new ArrayList<Player>();
+        // create array of players.
+        this.players = new ArrayList<Player>();
 
-		for (String currentName : inPlayers) {
-			players.add(new Player(currentName));
-		}
+        for (String currentName : inPlayers) {
+            players.add(new Player(currentName));
+        }
 
-	}
+    }
 
-	public PlayerController(Map<String, PlayerIcon> inPlayers) {
-		this.numberOfPlayer = inPlayers.size();
+    public PlayerController(Map<String, PlayerIcon> inPlayers) {
+        this.numberOfPlayer = inPlayers.size();
 
-		// assert wrong number.
-		if (!MonopolyUtils.verifyPlayerNumber(numberOfPlayer)) {
-			throw new AssertionError("Ung�ltige Anzahl an Spielern.");
-		}
-		
-		// create array of players.
-		this.players = new ArrayList<Player>();
-		
-		for (String current: inPlayers.keySet()){
-			players.add(new Player(current, inPlayers.get(current)));
-		}
-		
-	}
+        // assert wrong number.
+        if (!MonopolyUtils.verifyPlayerNumber(numberOfPlayer)) {
+            throw new AssertionError("Ung�ltige Anzahl an Spielern.");
+        }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Player getNextPlayer() {
-		currentPlayer = (currentPlayer + 1) % numberOfPlayer;
-		return getCurrentPlayer();
-	}
+        // create array of players.
+        this.players = new ArrayList<Player>();
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Player getFirstPlayer() {
-		currentPlayer = IMonopolyUtil.FIRST_PLAYER;
-		return getCurrentPlayer();
-	}
+        for (String current : inPlayers.keySet()) {
+            players.add(new Player(current, inPlayers.get(current)));
+        }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Player getCurrentPlayer() {
-		return players.get(currentPlayer);
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public int getNumberOfPlayer() {
-		return numberOfPlayer;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Player getNextPlayer() {
+        currentPlayer = (currentPlayer + 1) % numberOfPlayer;
+        return getCurrentPlayer();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Player getPlayer(int i) {
-		return players.get(i);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Player getFirstPlayer() {
+        currentPlayer = IMonopolyUtil.FIRST_PLAYER;
+        return getCurrentPlayer();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void transferMoney(Player currentPlayer, ICards currentCard) {
-		int actualMoney;
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Player getCurrentPlayer() {
+        return players.get(currentPlayer);
+    }
 
-		// parse card for the money
-		try {
-			actualMoney = Integer.parseInt(currentCard.getTarget());
-		} catch (NumberFormatException e) {
-			throw new AssertionError("String ist keine Zahl" + e);
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNumberOfPlayer() {
+        return numberOfPlayer;
+    }
 
-		// transfer money between bank and player.
-		if (currentCard.isReceiveFromToPark()) {
-			if (actualMoney < 0) {
-				Bank.addParkingMoney(currentPlayer, -actualMoney);
-			} else {
-				Bank.receiveMoney(currentPlayer, actualMoney);
-			}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Player getPlayer(int i) {
+        return players.get(i);
+    }
 
-			// transfer money between players (including the player itself,
-			// because it doesn't matter)
-		} else {
-			for (Player tempPlayer : players) {
-				Bank.receiveMoneyFromPlayer(currentPlayer, tempPlayer,
-						actualMoney);
-			}
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void transferMoney(Player currentPlayer, ICards currentCard) {
+        int actualMoney;
+
+        // parse card for the money
+        try {
+            actualMoney = Integer.parseInt(currentCard.getTarget());
+        } catch (NumberFormatException e) {
+            throw new AssertionError("String ist keine Zahl" + e);
+        }
+
+        // transfer money between bank and player.
+        if (currentCard.isReceiveFromToPark()) {
+            if (actualMoney < 0) {
+                Bank.addParkingMoney(currentPlayer, -actualMoney);
+            } else {
+                Bank.receiveMoney(currentPlayer, actualMoney);
+            }
+
+            // transfer money between players (including the player itself,
+            // because it doesn't matter)
+        } else {
+            for (Player tempPlayer : players) {
+                Bank.receiveMoneyFromPlayer(currentPlayer, tempPlayer,
+                        actualMoney);
+            }
+        }
+    }
 
 }
