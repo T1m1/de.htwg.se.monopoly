440d82089cca9092c667d9e1574ff72536f4cd6c
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-04-08 12:03:46 +0200

    introduced new DAO interface and an empty implementation of Db4o

---
+++ HtwgMonopoly/src/de/htwg/monopoly/database/IMonopolyDAO.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.htwg.monopoly.database;
+
+/**
+ * @author Steffen
+ *
+ */
+public interface IMonopolyDAO {
+	
+	boolean create();
+	
+	boolean read();
+	
+	boolean update();
+	
+	boolean delete();
+
+}
---
+++ HtwgMonopoly/src/de/htwg/monopoly/database/impl/Db4oDAO.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package de.htwg.monopoly.database.impl;
+
+import de.htwg.monopoly.database.IMonopolyDAO;
+
+/**
+ * @author Steffen
+ *
+ */
+public class Db4oDAO implements IMonopolyDAO {
+
+	/**
+	 * 
+	 */
+	public Db4oDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see de.htwg.monopoly.database.IMonopolyDAO#create()
+	 */
+	@Override
+	public boolean create() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.htwg.monopoly.database.IMonopolyDAO#read()
+	 */
+	@Override
+	public boolean read() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.htwg.monopoly.database.IMonopolyDAO#update()
+	 */
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.htwg.monopoly.database.IMonopolyDAO#delete()
+	 */
+	@Override
+	public boolean delete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
