c65e1e15288a47b88715824a61f21e7ac4f90797
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-04-13 17:37:34 +0200

    introduce new Interface for the Game Context

--- HtwgMonopoly/src/de/htwg/monopoly/context/IMonopolyGame.java
+++ HtwgMonopoly/src/de/htwg/monopoly/context/IMonopolyGame.java
@@ -1,5 +1,28 @@
 package de.htwg.monopoly.context;
 
+import de.htwg.monopoly.controller.IPlayerController;
+import de.htwg.monopoly.controller.IPlayfield;
+import de.htwg.monopoly.entities.impl.PrisonQuestion;
+import de.htwg.monopoly.util.GameStatus;
+
 public interface IMonopolyGame {
+
+	IPlayfield getPlayfield();
+
+	IPlayerController getPlayerController();
+
+	PrisonQuestion getPrisonQuestions();
+
+	GameStatus getCurrentGamePhase();
+
+	void setPlayfield(IPlayfield field);
+
+	void setPlayerController(IPlayerController players);
+
+	void setPrisonQuestions(PrisonQuestion questions);
+
+	void setCurrentGamePhase(GameStatus currentPhase);
+
+	String getId();
 
 }
