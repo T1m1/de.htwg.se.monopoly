1f2672868c81afccea0231ef65179e918e9b5f93
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-04-24 11:22:24 +0200

    remove final from class prison questions, since mockito cant handle final classes

--- HtwgMonopoly/src/de/htwg/monopoly/entities/impl/PrisonQuestion.java
+++ HtwgMonopoly/src/de/htwg/monopoly/entities/impl/PrisonQuestion.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public final class PrisonQuestion {
+public class PrisonQuestion {
 
 	private static final Map<String, Boolean> QUESTIONMAP = new TreeMap<String, Boolean>();
 
