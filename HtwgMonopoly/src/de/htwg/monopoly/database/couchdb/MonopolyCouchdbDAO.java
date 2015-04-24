0bf46fc3769f59dcb1ab1bb48765f3d816120916
T1m1 <Ruprecht.t@gmx.de>
2015-04-24 12:27:47 +0200

    #7 implement monopolyDAO interface

--- HtwgMonopoly/src/de/htwg/monopoly/database/couchdb/MonopolyCouchdbDAO.java
+++ HtwgMonopoly/src/de/htwg/monopoly/database/couchdb/MonopolyCouchdbDAO.java
@@ -1,8 +1,42 @@
 package de.htwg.monopoly.database.couchdb;
 
+import de.htwg.monopoly.context.IMonopolyGame;
+import de.htwg.monopoly.database.IMonopolyDAO;
+
+import java.util.List;
+
 /**
  * Created by Timi on 24.04.2015.
  */
-public class MonopolyCouchdbDAO {
+public class MonopolyCouchdbDAO implements IMonopolyDAO {
 
+    @Override
+    public void saveGame(IMonopolyGame context) {
+
+    }
+
+    @Override
+    public IMonopolyGame getGameById(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteGameById(String id) {
+
+    }
+
+    @Override
+    public void updateGame(IMonopolyGame game) {
+
+    }
+
+    @Override
+    public List<IMonopolyGame> getAllGames() {
+        return null;
+    }
+
+    @Override
+    public boolean containsGameById(String id) {
+        return false;
+    }
 }
