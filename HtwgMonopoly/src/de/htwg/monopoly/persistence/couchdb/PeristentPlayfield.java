2494f4363b94871e253fb83c0c2a51eca68cf73a
T1m1 <Ruprecht.t@gmx.de>
2015-05-01 10:02:42 +0200

    add Persitent Playfield class

---
+++ HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PeristentPlayfield.java
@@ -0,0 +1,10 @@
+package de.htwg.monopoly.persistence.couchdb;
+
+import org.ektorp.support.CouchDbDocument;
+
+/**
+ * @author Timi.
+ */
+public class PeristentPlayfield  extends CouchDbDocument{
+    
+}
