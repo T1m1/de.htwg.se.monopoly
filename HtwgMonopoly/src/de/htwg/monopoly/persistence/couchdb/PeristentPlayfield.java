ab6a7579cf8c3fde622e8840455eceea2446f618
T1m1 <Ruprecht.t@gmx.de>
2015-05-01 10:11:17 +0200

    implement lombok getter and setter

--- HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PeristentPlayfield.java
+++ HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PeristentPlayfield.java
@@ -1,13 +1,24 @@
 package de.htwg.monopoly.persistence.couchdb;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.ektorp.support.CouchDbDocument;
 
 /**
  * @author Timi.
  */
 public class PeristentPlayfield  extends CouchDbDocument{
+    
+    @Getter
+    @Setter
     private Integer numberOfFields;
+    @Getter
+    @Setter
     private Integer currentPlayer;
+    @Getter
+    @Setter
     private Integer parkingMoney;
+    @Getter
+    @Setter
     private String GamePhase;
 }
