9048343562b4e356230264e0852bf219f62acfb8
T1m1 <Ruprecht.t@gmx.de>
2015-05-01 12:14:41 +0200

    implement game class for couchdb

--- HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PersistentGame.java
+++ HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PersistentGame.java
@@ -1,9 +1,21 @@
 package de.htwg.monopoly.persistence.couchdb;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.ektorp.support.CouchDbDocument;
+
+import java.util.Collection;
 
 /**
  * @author Timi,
  */
-public class PersistentGame extends CouchDbDocument{
+public class PersistentGame extends CouchDbDocument {
+
+    @Getter
+    @Setter
+    Collection<PersistenUser> user;
+    
+    @Getter
+    @Setter
+    PeristentPlayfield playfield;
 }
