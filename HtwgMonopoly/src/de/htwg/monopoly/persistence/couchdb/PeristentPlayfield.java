20cd6d056a0cfb4502a7204e20e625768ccb03fc
T1m1 <Ruprecht.t@gmx.de>
2015-05-01 10:10:35 +0200

    add structure of pojo

--- HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PeristentPlayfield.java
+++ HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PeristentPlayfield.java
@@ -6,5 +6,8 @@ import org.ektorp.support.CouchDbDocument;
  * @author Timi.
  */
 public class PeristentPlayfield  extends CouchDbDocument{
-    
+    private Integer numberOfFields;
+    private Integer currentPlayer;
+    private Integer parkingMoney;
+    private String GamePhase;
 }
