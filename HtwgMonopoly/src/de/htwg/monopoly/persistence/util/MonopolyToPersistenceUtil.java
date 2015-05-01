682d1b6133a07636c15b8655476de075016cd13e
T1m1 <Ruprecht.t@gmx.de>
2015-05-01 12:51:14 +0200

    implement function to tramform game for couchdb

--- HtwgMonopoly/src/de/htwg/monopoly/persistence/util/MonopolyToPersistenceUtil.java
+++ HtwgMonopoly/src/de/htwg/monopoly/persistence/util/MonopolyToPersistenceUtil.java
@@ -1,7 +1,33 @@
 package de.htwg.monopoly.persistence.util;
 
+import de.htwg.monopoly.context.IMonopolyGame;
+import de.htwg.monopoly.persistence.couchdb.PeristentPlayfield;
+import de.htwg.monopoly.persistence.couchdb.PersistenUser;
+import de.htwg.monopoly.persistence.couchdb.PersistentGame;
+
+import java.util.Collection;
+
 /**
- * Created by Timi on 01.05.2015.
+ * @author Timi.
  */
 public class MonopolyToPersistenceUtil {
+    
+    public PersistentGame transformToCouchDb(IMonopolyGame game) {
+        PersistentGame persistentGame= new PersistentGame();
+
+        // Playfield
+        // TODO:
+        // - id
+        // - currentPlayer (int or name)
+        PeristentPlayfield peristentPlayfield = new PeristentPlayfield();
+        peristentPlayfield.setNumberOfFields(game.getPlayfield().getfieldSize());
+        peristentPlayfield.setGamePhase(game.getCurrentGamePhase().toString());
+        peristentPlayfield.setParkingMoney(game.getParkingMoney());
+        
+
+        Collection<PersistenUser> persistenUser;
+        
+        
+        return persistentGame;
+    }
 }
