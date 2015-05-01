01a34bff09ef0cea396f750bad70e201e3d15d8b
T1m1 <Ruprecht.t@gmx.de>
2015-05-01 09:25:41 +0200

    variables for persistent user

--- HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PersistenUser.java
+++ HtwgMonopoly/src/de/htwg/monopoly/persistence/couchdb/PersistenUser.java
@@ -1,10 +1,23 @@
 package de.htwg.monopoly.persistence.couchdb;
 
+import de.htwg.monopoly.entities.IFieldObject;
+import de.htwg.monopoly.util.PlayerIcon;
 import org.ektorp.support.CouchDbDocument;
+
+import java.util.List;
 
 /**
  * @author Timi.
  */
 public class PersistenUser extends CouchDbDocument{
+    
+    private String name;
+    private int budget;
+    private int position;
+    private int prisonRound;
+    private boolean inPrison;
+    private List<IFieldObject> ownership;
+    private int prisonFreeCard;
+    private PlayerIcon icon;
     
 }
