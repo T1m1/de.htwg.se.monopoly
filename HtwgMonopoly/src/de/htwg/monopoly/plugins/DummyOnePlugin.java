a564a944454b6701d3f38ebf8c7c348a712071f2
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-24 13:04:32 +0200

    implement plugins

--- HtwgMonopoly/src/de/htwg/monopoly/plugins/DummyOnePlugin.java
+++ HtwgMonopoly/src/de/htwg/monopoly/plugins/DummyOnePlugin.java
@@ -3,6 +3,13 @@
  */
 package de.htwg.monopoly.plugins;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,32 +20,53 @@ import de.htwg.monopoly.util.GameStatus;
  * @author Steffen
  *
  */
-public class DummyOnePlugin implements MonopolyPlugin {
+public class DummyOnePlugin extends JFrame implements MonopolyPlugin {
 
 	private IController controller;
 	private final Logger logger = LogManager.getLogger("DummyOne");
 
-
+	protected JLabel label;
 	@Override
 	public void update(GameStatus e) {
-		logger.info("DummyOne got status: " + e);
+		label.setText("Internal Game Status: " + e);
 	}
 
 	@Override
 	public String getName() {
 		return "DummyPlugin 1";
 	}
-
+	
 	@Override
 	public void enable(IController controller) {
-		this.controller = controller;
-		logger.info("DummyOne got enabled");
+		/** frame options **/
+		setTitle("HTWG Monopoly");
+		/* set minimum size */
+		Dimension dimension = new Dimension(200,200);
+		setMaximumSize(dimension);
+		setMinimumSize(dimension);
+		setPreferredSize(dimension);
+
+		setSize(200,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		label = new JLabel();
+
+	    label.setFont( new Font("Arial", Font.CENTER_BASELINE, 30));
+		
+		add(label, BorderLayout.CENTER);
+		
+		label.setText("Start Plugin 1");
+		
+		setVisible(true);
+
 
 	}
-
+	
 	@Override
 	public void disable() {
-		logger.info("DummyOne got disabled");
+		setVisible(false);
+
 	}
 
 }
--- HtwgMonopoly/src/de/htwg/monopoly/plugins/DummyTwoPlugin.java
+++ HtwgMonopoly/src/de/htwg/monopoly/plugins/DummyTwoPlugin.java
@@ -1,36 +1,71 @@
 package de.htwg.monopoly.plugins;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import de.htwg.monopoly.controller.IController;
 import de.htwg.monopoly.util.GameStatus;
 
-public class DummyTwoPlugin implements MonopolyPlugin {
+public class DummyTwoPlugin extends JFrame implements MonopolyPlugin {
 
 	private IController controller;
-	private final Logger logger = LogManager.getLogger("DummyTwo");
+	private JButton button;
+	private JTextArea taOutput;
+	private final Logger logger = LogManager.getLogger("DummyOne");
+
+	protected JLabel label;
+	private JPanel panel;
 
 	@Override
 	public void update(GameStatus e) {
-		logger.info("DummyTwo got status" + e);
+		label.setText("Spieler-Optionen:" + controller.getOptions());
 	}
 
 	@Override
 	public String getName() {
-		return "DummyPlugin 2";
+		return "DummyPlugin 1";
 	}
 
 	@Override
 	public void enable(IController controller) {
 		this.controller = controller;
-		logger.info("DummyTwo got enabled");
+		/** frame options **/
+		setTitle("HTWG Monopoly");
+		/* set minimum size */
+		Dimension dimension = new Dimension(200, 200);
+		setMaximumSize(dimension);
+		setMinimumSize(dimension);
+		setPreferredSize(dimension);
+
+		setSize(200, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		label = new JLabel();
+
+		label.setFont(new Font("Arial", Font.CENTER_BASELINE, 25));
+
+		add(label, BorderLayout.CENTER);
+
+		label.setText("Start Plugin 2");
+
+		setVisible(true);
 
 	}
 
 	@Override
 	public void disable() {
-		logger.info("DummyTwo got disabled");
-	}
+		setVisible(false);
 
+	}
 }
