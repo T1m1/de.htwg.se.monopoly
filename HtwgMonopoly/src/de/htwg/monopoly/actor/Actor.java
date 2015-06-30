051cfb617600392a915169168c5c68813ddd76fe
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-30 23:41:16 +0200

    add Actor class

---
+++ HtwgMonopoly/src/de/htwg/monopoly/actor/Actor.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.htwg.monopoly.actor;
+
+import akka.actor.UntypedActor;
+
+/**
+ * @author Steffen
+ *
+ */
+public class Actor extends UntypedActor {
+
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
