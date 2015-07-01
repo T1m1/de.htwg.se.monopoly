6436876860202215e87672a40da49db9aa1e9382
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-07-01 09:58:10 +0200

    add logger and unhandle message if not handled

--- HtwgMonopoly/src/de/htwg/monopoly/actor/Actor.java
+++ HtwgMonopoly/src/de/htwg/monopoly/actor/Actor.java
@@ -3,6 +3,9 @@
  */
 package de.htwg.monopoly.actor;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.htwg.monopoly.util.GameStatus;
 import akka.actor.UntypedActor;
 
@@ -12,18 +15,31 @@ import akka.actor.UntypedActor;
  */
 public class Actor extends UntypedActor {
 	
+
+    /* logger */
+    private final Logger logger = LogManager.getLogger("htwgMonopoly");
+	
 	public Actor() {
+		logger.info("Actor instanciated");
 		
 	}
 
 	@Override
 	public void onReceive(Object msg) throws Exception {
+		
+		logger.info("Actor received message");
+		
+		
 
 		if (msg instanceof ActorMessage &&
 				"update" == ((ActorMessage) msg).getMessage()) {
 			
+			logger.info("Actor will update all Observers");
+			
 			GameStatus phase = ((ActorMessage) msg).getGameStatus();
 			((ActorMessage) msg).getController().notifyObservers(phase);
+		} else {
+			unhandled(msg);
 		}
 	}
 
